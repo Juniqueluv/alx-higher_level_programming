17b1ad68cec62c689a06ea503fceb40966262636
juniqueluv <oolajumoke099@gmail.com>
2022-10-10 12:32:34 -0700

    errors fixed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,6 +15,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
+    try/except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
